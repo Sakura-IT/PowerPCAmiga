f3bf456c1655223a2b808fecdcbd4f7c930ff52e
Dennis Boon <dennisvdboon@gmail.com>
2019-10-13 09:17:14 +0000

    Added kernel files, updated makefile.

---
+++ kernel.c
@@ -0,0 +1,111 @@
+ // Copyright (c) 2019 Dennis van der Boon
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+
+__section ("kernel","acrx") __entry __interrupt void ExcFPUnav(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcAlignment(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcInsStor(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcDatStor(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcTrace(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcBreakPoint(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcDec(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcPrIvate(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcMachCheck(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcSysCall(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcPerfMon(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcSysMan(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcTherMan(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcVMXUnav(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcITLBMiss(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcDLoadTLBMiss(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcDStoreTLBMiss(void)
+{
+    return;
+}
+
+__section ("kernel","acrx") __entry __interrupt void ExcExternal(void)
+{
+    return;
+}
